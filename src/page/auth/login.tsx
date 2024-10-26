a8fabbe3c8886fcfeb9b65fbca506487c5743490
Yorukot <ben.hrc57@gmail.com>
2024-10-27 02:20:37 +0800

    update login.tsx

--- src/page/auth/login.tsx
+++ src/page/auth/login.tsx
@@ -1,7 +1,3 @@
 import type { Component } from "solid-js";
 
-import styles from "@/src/App.module.css";
-import "@/src/index.css";
-import * as m from "@/src/paraglide/messages.js";
-import { setLanguageTag } from "@/src/paraglide/runtime";
-import ToggleTheme from "@/src/components/ToggleTheme";
+import styles from "./login.module.css";
