615b29124c3fba03454caf830708ab9cf40f3be5
Yorukot <ben.hrc57@gmail.com>
2024-10-26 23:10:47 +0800

    Fix lucide icon issue

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -1,7 +1,10 @@
 import type { Component } from "solid-js";
 import { createSignal, onMount } from "solid-js";
-import { Moon, Sun } from "lucide-solid";
+
 import styles from "./ToggleTheme.module.css";
+
+import Moon from "lucide-solid/icons/moon";
+import Sun from "lucide-solid/icons/sun";
 
 const ToggleTheme: Component = () => {
   const [isDark, setIsDark] = createSignal(true);
@@ -29,7 +32,6 @@ const ToggleTheme: Component = () => {
 
   return (
     <div class={styles.toggleThemeIcon} onClick={toggleTheme}>
-      {/* FIXME: somehow this casue really slow rendering In dev tools this image loading about 3000 times */}
       {isDark() ? <Moon /> : <Sun />}
     </div>
   );
