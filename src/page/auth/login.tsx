e8f3e520fac0f16f0286f1153c205e14e048bf63
MapleML <luck2000331@gmail.com>
2024-10-27 14:58:40 +0800

    madol

--- src/page/auth/login.tsx
+++ src/page/auth/login.tsx
@@ -1,3 +1,13 @@
 import type { Component } from "solid-js";
 
 import styles from "./login.module.css";
+    
+      return (
+        <div class="container">
+
+          </div>
+ 
+      );
+
+    
+    export default login;
