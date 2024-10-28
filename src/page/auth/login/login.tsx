8205242586ae2968e3a30eeb9667f5529bc03885
Yorukot <ben.hrc57@gmail.com>
2024-10-28 08:20:48 +0800

    Update login hover and add google github gitlab icon

--- src/page/auth/login/login.tsx
+++ src/page/auth/login/login.tsx
@@ -3,26 +3,73 @@ import type { Component } from "solid-js";
 import styles from "./login.module.css";
 
 import { changeLocal, Locale, useTranslate } from "@/src/utils/i18n";
-import SelectLanguage from "@/src/components/selectLanguage/selectLanguage";
-import ToggleTheme from "@/src/components/toggleTheme/ToggleTheme";
 
 const LoginModal: Component = () => {
-
   const t = useTranslate();
 
   return (
     <div class={styles.loginBox}>
-      <div class={`${styles.title} ${styles["title"]}`}>{t("auth.login.Title")}</div>
-      <button class={`${styles.button} ${styles["loginGoogle"]}`}>{t("auth.login.Google")}</button>
-      <button class={`${styles.button} ${styles["loginGitlab"]}`}>{t("auth.login.GitLab")}</button>
-      <button class={`${styles.button} ${styles["loginGithub"]}`}>{t("auth.login.GitHub")}</button>
+      {/* Button */}
+      <div class={`${styles.title} ${styles["title"]}`}>
+        {t("auth.login.Title")}
+      </div>
+
+      <button class={`${styles.button} ${styles.loginGoogle}`}>
+        <img
+          src="/src/assets/svg/google.svg"
+          alt="Google icon"
+          class={styles.icon}
+        />
+        {t("auth.login.Google")}
+      </button>
+
+      <button class={`${styles.button} ${styles.loginGithub}`}>
+        <img
+          src="/src/assets/svg/github.svg"
+          alt="Google icon"
+          class={styles.icon}
+        />
+        {t("auth.login.GitHub")}
+      </button>
+
+      <button class={`${styles.button} ${styles.loginGitlab}`}>
+        <img
+          src="/src/assets/svg/gitlab.svg"
+          alt="Google icon"
+          class={styles.icon}
+        />
+        {t("auth.login.GitLab")}
+      </button>
+
+      {/* Line */}
       <div class={styles.lineWrapper}>
-  <div class={styles.line}><svg xmlns="http://www.w3.org/2000/svg" width="125" height="2" viewBox="0 0 125 2" fill="none"><path d="M0 1H125" stroke="var(--text)" /></svg></div>
-  <div class={styles.linetext}>or</div>
-  <div class={styles.line}><svg xmlns="http://www.w3.org/2000/svg" width="125" height="2" viewBox="0 0 125 2" fill="none"><path d="M0 1H125" stroke="var(--text)" /></svg></div>
-</div>
+        <div class={styles.line}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="125"
+            height="2"
+            viewBox="0 0 125 2"
+            fill="none"
+          >
+            <path d="M0 1H125" stroke="var(--text)" />
+          </svg>
+        </div>
+        <div class={styles.linetext}>or</div>
+        <div class={styles.line}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="125"
+            height="2"
+            viewBox="0 0 125 2"
+            fill="none"
+          >
+            <path d="M0 1H125" stroke="var(--text)" />
+          </svg>
+        </div>
+      </div>
+      {/* Email login */}
     </div>
   );
 };
 
-export default LoginModal 
+export default LoginModal;
